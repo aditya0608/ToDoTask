9215403e823c404350a27f7790f8407f13d5df83
aditya0608 <aditya.cm60@gmail.com>
2022-05-19 14:16:02 +0530

    Initial commit

---
+++ src/test/java/com/aditya/todotask/ToDoTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aditya.todotask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
